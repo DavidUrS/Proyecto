9f8d691d74b8a71cb2a5c213898f345ac377f19e
David Uribe <uribesotod@gmil.com>
2017-09-26 19:15:03 -0500

    Nuevo Servicio

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -4,19 +4,32 @@ import {Http} from "@angular/http";
 @Injectable()
 export class InformacionService {
 
-  info:any = {};
-  cargada:boolean = false;
+  public info:any = {};
+  public cargada:boolean = false;
+  public cargada_sobre_nosotros:boolean = false;
+  public equipo:any[] = [];
 
   constructor(public http:Http) {
+  this.carga_info();
+  this.carga_sobre_nosotros();
+ }
 
-  this.http.get("assets/data/info.pagina.json")
-              .subscribe(data =>{
-                this.info = data.json();
-                console.log(this.info);
-                this.cargada = true;
+  public carga_info(){
+    this.http.get("assets/data/info.pagina.json")
+                .subscribe(data =>{
+                  this.info = data.json();
+                  this.cargada = true;
+                  // console.log(this.info);
+                })
+  }
 
-              })
-
+  public carga_sobre_nosotros(){
+    this.http.get("https://mundo-web.firebaseio.com/Equipo.json")
+                .subscribe(data =>{
+                  this.equipo = data.json();
+                  this.cargada_sobre_nosotros = true;
+                  console.log(this.equipo);
+                })
   }
 
 }
